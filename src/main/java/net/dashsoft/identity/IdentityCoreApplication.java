acbb8fce9da8e46f2d00bdfe05755f1384b983de
julian <julian@macbook-air.local>
2025-07-13 19:46:28 +0200

    (DAS-10) [INIT] Bootstrap IdentityCore Spring Boot Project

---
+++ src/main/java/net/dashsoft/identity/IdentityCoreApplication.java
@@ -0,0 +1,13 @@
+package net.dashsoft.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/dashsoft/identity/IdentityCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dashsoft.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
